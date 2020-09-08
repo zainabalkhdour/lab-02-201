b6c37598492d4328932e41a39b6fc3139d954201
MsDiala <diala-is-diala@hotmail.com>
2020-09-08 03:28:19 +0300

    add

---
+++ js/app.js
@@ -0,0 +1,84 @@
+'use strict'
+var userInput = prompt("what is your name?")
+alert("welcome"+userInput)
+
+'use strict'
+var userInput = prompt("what is your name?")
+alert("welcome"+userInput)
+
+  "yes"== true;
+  "y"== true ;
+             "no"==false;
+             "n"== false
+
+var q1 = prompt("Do I like to do sports?(yes/no)")
+if (q1.toLocaleLowerCase()=="yes")
+{
+        alert("right answer")
+        console.log("right")
+}
+else 
+{
+
+    alert("wrong answer")
+    console.log("Wrong")
+
+}
+
+var q2 = prompt("My favorit food is mansaf")
+if(q2.toLocaleLowerCase()=="yes")
+{
+    alert("right answer")
+    console.log("right")
+
+}
+else
+{
+    alert("wrong answer")
+    console.log("wrong")
+
+}
+
+var q3 = prompt("Am I a bad singer?")
+if(q3.toLocaleLowerCase()=="yes")
+{
+    alert("wrong answer")
+    console.log("wrong")
+
+}
+else{
+    alert("right answer")
+    console.log("right")
+
+}
+
+
+var q4 = prompt("Do I hate winter")
+if (q4.toLocaleLowerCase()=="yes")
+{
+        alert("wrong answer")
+        console.log("wrong")
+}
+else 
+{
+
+    alert("right answer")
+    console.log("right")
+
+}
+
+var q5 = prompt("Am I a good coocker?")
+if (q5.toLocaleLowerCase()=="yes")
+{
+        alert("right answer")
+        console.log("right")
+}
+else 
+{
+
+    alert("wrong answer")
+    console.log("wrong")
+
+}
+
+
