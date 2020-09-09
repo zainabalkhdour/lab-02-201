af0e2467730abb53461e59e41a23d66d9b7ce3aa
MsDiala <70090838+MsDiala@users.noreply.github.com>
2020-09-09 03:59:07 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
 'use strict'
+var numberofcorrectanswoer= 0;
+
 var userInput = prompt("what is your name?")
 alert("welcome"+userInput)
 
-
   "yes"== true;
   "y"== true ;
-             "no"==false;
-             "n"== false
+      "no"==false;
+     "n"== false
 
 var q1 = prompt("Do I like to do sports?(yes/no)")
-if (q1.toLocaleLowerCase()==="yes")
+if (q1.toLocaleLowerCase()=="yes")
 {
+    numberofcorrectanswoer++;
         alert("right answer")
         console.log("right")
 }
@@ -23,8 +25,9 @@ else
 }
 
 var q2 = prompt("My favorit food is mansaf")
-if(q2.toLocaleLowerCase()==="yes")
+if(q2.toLocaleLowerCase()=="yes")
 {
+    numberofcorrectanswoer++;
     alert("right answer")
     console.log("right")
 
@@ -37,13 +40,16 @@ else
 }
 
 var q3 = prompt("Am I a bad singer?")
-if(q3.toLocaleLowerCase()==="yes")
+if(q3.toLocaleLowerCase()=="yes")
 {
+    
     alert("wrong answer")
     console.log("wrong")
 
 }
 else{
+
+    numberofcorrectanswoer++;
     alert("right answer")
     console.log("right")
 
@@ -51,22 +57,23 @@ else{
 
 
 var q4 = prompt("Do I hate winter")
-if (q4.toLocaleLowerCase()==="yes")
+if (q4.toLocaleLowerCase()=="yes")
 {
         alert("wrong answer")
         console.log("wrong")
 }
 else 
 {
-
+    numberofcorrectanswoer++;
     alert("right answer")
     console.log("right")
 
 }
 
 var q5 = prompt("Am I a good coocker?")
-if (q5.toLocaleLowerCase()==="yes")
+if (q5.toLocaleLowerCase()=="yes")
 {
+    numberofcorrectanswoer++;
         alert("right answer")
         console.log("right")
 }
@@ -78,4 +85,71 @@ else
 
 }
 
+
+
+var numberOftrial= 0; 
+while(numberOftrial <4){
+    var q6 = parseInt(prompt("My age is :"));
+    if(q6==23){
+        alert("your answer is true ");
+        numberofcorrectanswoer++;
+        break;
+    }
+     else if(q6<23){
+        alert("your answer is too low ");
+        numberOftrial++;
+     }
+     else if(q6>23){
+        alert("your answer is too high  ");
+        numberOftrial++;
+     }
+     if(numberOftrial ==3){
+        alert(" OUT OF CHANCES "); 
+        break;
+   
+     }
+    
+}
+
+
+
+var hoppies = ["singing","reading","dancing","coocking","kickboxing"];
+
+var numberoftrail=0;
+while( numberoftrail < 6) {
+    debugger;
+   var q7 = prompt("Guess type one of my hoppies ").toLowerCase();
+   var result= true;
+   for(var j = 0 ;j<hoppies.length ;j++){
+      
+    if(q7==hoppies[j]){
+        result= true;
+     break;
+    }else{
+        result=false;
+    }
+   }
+
+ if(result==true){
+    alert("you are totaly right "+q7+"is one of my hoppies ");
+    numberofcorrectanswoer++;
+    break;
+
+ }
+   if(result===false){
+    alert(q7+"is not one of my hoppies ");
+   }
+  
+
+   if(numberoftrail ==5){
+    alert("OUT OF CHANCES ");
+
+    }
+numberoftrail++;
+
+}
+
+alert("the possible correct answers are["+"singing"+","+"reading"+","+"dancing"+","+"coocking"+","+"kickboxing"+"]")
+alert("Your result is "+numberofcorrectanswoer++ +"/7")
+ 
 
